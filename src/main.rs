a449143c33bc4021a7982bdc72010efc7c2b356c
laopo001 <353272497@qq.com>
2019-05-27 19:05:45 +0800

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::f32::consts::PI;
+use wasm_math::vec3::Vec3;
+use rand::Rng;
+
+struct Ray {
+	pub origin: Vec3,
+	pub direct: Vec3,
+}
+
+impl Ray {
+	pub fn new(origin: Vec3, direct: Vec3) -> Self {
+		Ray { origin, direct }
+	}
+}
+
+enum Material {
+	Diffuse,
+	Specular,
+	Refract,
+}
+
+struct Sphere {
+	radius: f32,
+	position: Vec3,
+	emission: Vec3,
+	color: Vec3,
+	material: Material,
+}
+
+impl Sphere {
+	pub fn new(radius: f32, position: Vec3, emission: Vec3, color: Vec3, material: Material) -> Self {
+		Sphere {
+			radius,
+			position,
+			emission,
+			color,
+			material,
+		}
+	}
+	pub fn intersect(&self, ray: &Ray) -> f32 {
+//		let op = self.position.clone().sub(&ray.origin);
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	let mut rng = rand::thread_rng();
+	println!("Hello, world!,{}", rng.gen::<f32>());
 }
